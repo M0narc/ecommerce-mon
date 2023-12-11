88f17e13ccf13ada0e50b86ece20e447ab0cf4eb
Franco Lujan <francomatiaslujan23@gmail.com>
2023-12-11 19:15:35 -0300

    e-com template base

--- moon/urls.py
+++ moon/urls.py
@@ -22,6 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('user/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
