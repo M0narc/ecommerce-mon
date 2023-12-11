7ed613ea5656bed26109f213edbf1626909307ba
Franco Lujan <francomatiaslujan23@gmail.com>
2023-12-11 17:30:26 -0300

    User model, admin, url, MEDIA_URL, MEDIA_ROOT

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    gender_choice = (
+        ('M', 'Man'),
+        ('F', 'Female'),
+    )
+
+    code = models.IntegerField()
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    first_name = models.CharField(null=False, max_length=100)
+    last_name = models.CharField(null=True, max_length=100)
+    email = models.EmailField(null=True, unique=True, max_length=100)
+    profesion = models.CharField(max_length=100)
+    genero = models.CharField(choices=gender_choice, max_length=100)
+    city = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
